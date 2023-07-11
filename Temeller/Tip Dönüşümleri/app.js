9fdd63b1cc362c113b16f54ba5e29b3296d4c6ab
Eslem Filiz <eslemf58@gmail.com>
2023-07-11 16:16:17 +0300

    V.02

---
+++ Temeller/Tip Dönüşümleri/app.js
@@ -0,0 +1,109 @@
+// let value;
+//  Veri Tiplerini String'e Çevirme
+
+value= String(123);
+value=String(3.14);
+value=String(true);
+value=String(false);
+value=String(function( ){console.log()} );
+value =String([1,2,3,4]);
+
+value=(10).toString();
+value=3.14.toString();
+
+
+// console.log(value);
+// console.log(typeof value);
+
+// veri tiplerini sayılara çevirme
+
+// 
+
+
+// const a="Hello"+34;
+
+// console.log(a);
+// console.log(typeof a);
+
+// console.log(value);
+// console.log(typeof value);
+
+
+// Aritmetik Operatörler
+
+// let value;
+
+// const value1=10;
+
+// const value2=20 
+ 
+value= value1 + value2;
+value= value1 - value2; 
+value= value1 * value2;
+value= value1 / value2;
+value= value1 % value2;
+
+
+
+value =Math.PI;
+value=Math.E;
+
+value=Math.round(3.6);
+value=Math.round(3.5);
+value=Math.round(3.2);
+
+// // yukarı yuvarlıyor
+value=Math.ceil(3.2);
+value=Math.ceil(3.7);
+value=Math.ceil(3.2);
+ 
+// // aşağı yuvarlıyor
+value=Math.floor(3.2);
+value=Math.floor(3.7);
+
+// // karekök alma
+value=Math.sqrt(16);
+value=Math.sqrt(31);
+
+// // mutlak değer
+
+value=Math.abs(-10);
+
+// // üs alma
+value=Math.pow(8,3);
+value=Math.pow(4,3);
+
+// // max ve min metodu
+value=Math.max(10,-1,100,3);
+value=Math.min(10,-1,100,3);
+
+// //  0 ve 1 arasında random sayıyı üretir
+value=Math.random();
+
+// value=Math.random()*20;
+
+
+
+let value;
+ const firstName="Louis";
+ const lastName="Armstrong";
+
+ 
+ value= firstName  + " " +   lastName;
+
+//  value =firstName.toLowerCase();
+ value =firstName.toUpperCase();
+// console.log(value);
+
+
+
+// value= firstName[0];
+value=firstName[firstName.length-1];
+// console.log(value);
+
+
+// Index of
+
+value=firstName.indexOf("L");
+console.log(value);
+
---
+++ Temeller/Var,Let,Const/app.js
@@ -0,0 +1,40 @@
+// Var
+
+// var name= "Eslem";
+// console.log(name);
+
+// name = "Ebrar";
+// console.log(name);
+
+//  eror veriyor;
+// let name="Eslem";
+
+// comsole.log(name);
+
+// let name="Ebrar";
+
+// let a,b;
+
+// a=10;
+// b=20;
+// console.log(a,b );
+
+
+//  Const
+
+// const name=" Eslem";
+// console.log(name);
+
+// name="Pınar";
+// console.log(name);
+
+const a=[1,2,3,4,5];
+console.log(a);
+
+a.push(6);
+
+// const b=10;
+
+// b=20;
+
+console.log(a);
