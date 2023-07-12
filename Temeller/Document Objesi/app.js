8d5dcc5919d9b16ad693ecd8ca3a33e68dd89082
Eslem Filiz <eslemf58@gmail.com>
2023-07-12 17:52:48 +0300

    V.02

---
+++ Temeller/Document Objesi/app.js
@@ -0,0 +1,18 @@
+// Window Object
+let value;
+
+value=document;
+value=document.all;
+value=document.all.length;
+value = document.all[0];
+value = document.all[6];
+value = document.all[document.all.length-1];
+
+const elements = document.all;
+for(let i=0; i<elements.length; i++){
+    console.log(elements[i]);
+}
+
+
+
+// console.log(value);
---
+++ Temeller/To do lists/app.js
@@ -0,0 +1,52 @@
+// Element Id'e Göre Seçme
+
+// let element;
+
+// element=document.getElementById("todo-form");
+// element=document.getElementById("tasks-title");
+
+// E lement Class'a Göre Seçme
+
+// element = document.getElementByClassName("list-group-item");
+// element = document.getElementByClassName("card header");
+
+// // console.log(element);
+
+
+
+// // Element Tag'e Göre Seçme
+
+// element = document.getElementsByTagName("div");
+
+
+// // Query Selector-Css Selector- Tek Bir Element
+
+// element = document.querySelector("#todo-form");
+// element = document.querySelector("#tasks-title");
+
+// element = document.querySelector(".list-group-item");
+
+// element = document.querySelector("li");
+// element = document.querySelector("div");
+
+// // console.log(element);
+
+// const elements = document.querySelector("clear-todos");
+
+// // Element Özellikleri
+
+// // console.log(element.id);
+// // console.log(element);
+
+
+// // Style ve Element Özelliklerini Değiştirme
+
+
+
+// element.className="btn btn-success";
+// element.style.color = "primary";
+
+
+Element.textContent = "Silin";
+
+console.log(element);
